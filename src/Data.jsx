c89edec767d5fe527837d90e63b10025dcabf49c
anasmunawar2 <anasmunawar2@gmail.com>
2023-05-08 16:16:12 +0500

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Thomas",
+    email: "thomas@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Tom Cruise",
+    email: "tom@gmail.com",
+  },
+];
---
+++ src/Home.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteUser } from "./UserReducer";
+
+const Home = () => {
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(deleteUser({ id: id }));
+  };
+
+  return (
+    <div className="container my-5">
+      <h2>CRUD Opertaions using Redux Toolkit</h2>
+
+      <Link to="/create" className="btn btn-success my-3">
+        Create +
+      </Link>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>
+                <Link
+                  to={`/edit/${user.id}`}
+                  className="btn btn-sm btn-primary"
+                >
+                  Edit
+                </Link>
+                <button
+                  onClick={() => handleDelete(user.id)}
+                  className="btn btn-sm btn-danger ms-2"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Home;
